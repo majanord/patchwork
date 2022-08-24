601fdc1cc9481021c6550aa17bb51a539a53e2ab
Maja <majanord@gmail.com>
2022-08-24 22:01:32 +0200

    v1

---
+++ src/components/Fabric.vue
@@ -0,0 +1,32 @@
+<template>
+    <canvas id="canvas" width="300" height="300" ref="canvasel"></canvas>
+
+</template>
+<script>
+import { konva } from 'konva';
+
+export default {
+  name: 'fabricCanvas',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+    };
+  },
+  mounted() {
+    // const ref = this.$refs.canvasel;
+    // // const canvas = new konva.Canvas(ref);
+
+    // const triang = new fabric.Triangle({
+    //   top: 0,
+    //   left: 0,
+    //   width: 200,
+    //   height: 100,
+    //   fill: 'red',
+    //   rotatingPointOffset: 45,
+    // });
+    // canvas.add(triang);
+  },
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,51 +1,154 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress" target="_blank" rel="noopener">e2e-cypress</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-stage :config='configKonva'>
+    <v-layer>
+      <v-circle :config='configCircle'></v-circle>
+
+      <v-shape v-for="(p, i) in pwork" :key="i" :config="p" />
+    </v-layer>
+  </v-stage>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script >
 
-export default defineComponent({
+export default {
   name: 'HelloWorld',
   props: {
     msg: String,
   },
-});
+  data() {
+    return {
+      configKonva: {
+        width: 1000,
+        height: 1500,
+      },
+      configCircle: {
+        x: 100,
+        y: 100,
+        radius: 70,
+        fill: 'red',
+        stroke: 'black',
+        strokeWidth: 4,
+      },
+      pwork: this.patchwork(),
+    };
+  },
+  methods: {
+    patter() {
+      const size = 50;
+      const squareA = (en) => {
+        const a = { x: en.x, y: en.y };
+        const b = { x: en.x + size, y: en.y };
+        const c = { x: en.x, y: en.y + size };
+        const d = { x: en.x + size, y: en.y + size };
+        const triA1 = () => ({
+          start: a,
+          to: b,
+          tre: c,
+        });
+
+        const triA2 = () => ({
+          start: c,
+          to: d,
+          tre: b,
+        });
+
+        return [
+          triA1(), triA2(),
+        ];
+      };
+
+      const squareB = (en) => {
+        const a = { x: en.x, y: en.y };
+        const b = { x: en.x + size, y: en.y };
+        const c = { x: en.x, y: en.y + size };
+        const d = { x: en.x + size, y: en.y + size };
+
+        const triB1 = () => ({
+          start: a,
+          to: b,
+          tre: d,
+        });
+
+        const triB2 = () => ({
+          start: a,
+          to: c,
+          tre: d,
+        });
+
+        return [
+          triB1(), triB2(),
+        ];
+      };
+
+      let pattern = [];
+
+      for (let i = 0; i < 10; i += 1) {
+        console.log('row', i);
+        for (let j = 0; j < 15; j += 1) {
+          console.log('col', j);
+          console.log('A', i % 2 !== 0 && j % 2 === 0);
+          let check = true;
+          if (i % 2 === 0) {
+            check = (j % 2 === 0);
+          } else {
+            check = (j % 2 !== 0);
+          }
+          if (check) {
+            pattern = pattern.concat(squareA({ x: i * size, y: j * size }));
+          } else {
+            pattern = pattern.concat(squareB({ x: i * size, y: j * size }));
+          }
+        }
+      }
+      return pattern;
+    },
+    triangleConfig(start, to, tre, color) {
+      return {
+        sceneFunc: (context, shape) => {
+          context.beginPath();
+          context.moveTo(start.x, start.y);
+          context.lineTo(to.x, to.y);
+          context.lineTo(tre.x, tre.y);
+          context.closePath();
+
+          // special Konva.js method
+          context.fillStrokeShape(shape);
+        },
+        fill: color,
+        stroke: color,
+        strokeWidth: 1,
+      };
+    },
+    randomColor() {
+      const colors = [
+        '#C5FA94',
+        '#27BD42',
+        '#0B9851',
+        '#138152',
+        '#A1EAE6',
+        '#ACD9F0',
+        '#56B1E8',
+        '#21B9D0',
+        '#334C9F',
+        '#32405D', '#EAEAEA', '#E6DFCF', '#A5A5A5', '#7F59AC', '#A590B9', '#7A3288', '#FEC5D6', '#F297B8', '#FD7FA4', '#D3316C', '#E0DC71', '#ECBC5A', '#E9A13B', '#F09850', '#ECD468', '#F1613C', '#F0CFAF', '#D6AA83', '#DE4152', '#992E36',
+      ];
+
+      return colors[Math.floor(Math.random() * colors.length)];
+    },
+    patchwork() {
+      const patter = this.patter();
+      return patter.map((triangle) => {
+        const color = this.randomColor();
+        const out = this.triangleConfig(triangle.start, triangle.to, triangle.tre, color);
+        return out;
+      });
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped lang='scss'>
 h3 {
   margin: 40px 0 0;
 }
